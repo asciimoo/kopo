3bcb5b5b23db18d9a3f84fee6e8032d8b0cfacec
asciimoo <asciimoo@gmail.com>
2013-04-16 18:24:23 +0200

    [mod] data location change

--- collect_data.sh
+++ collect_data.sh
@@ -2,6 +2,6 @@
 
 curl 'http://torstatus.blutmagie.de/ip_list_exit.php/Tor_ip_list_EXIT.csv' -o torexits.csv
 
-curl 'http://0x2a.tk/GeoIPCity.dat' -o GeoIPCity.dat
+curl 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz' | gunzip > GeoIPCity.dat
 
 python whois.py init

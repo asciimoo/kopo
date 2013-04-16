83edbbde93be7a18f3471cf739797306d36a29bf
asciimoo <asciimoo@gmail.com>
2013-04-16 11:45:39 +0200

    [fix] download link

--- collect_data.sh
+++ collect_data.sh
@@ -2,6 +2,6 @@
 
 curl 'http://torstatus.blutmagie.de/ip_list_exit.php/Tor_ip_list_EXIT.csv' -o torexits.csv
 
-curl 'http://www.ctrlc.hu/~stef/GeoIPCity.dat' -o GeoIPCity.dat
+curl 'http://0x2a.tk/GeoIPCity.dat' -o GeoIPCity.dat
 
 python whois.py init
